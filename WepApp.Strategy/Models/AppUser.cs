ecad9c44793e18a6087b84fd80907b997e3d4a2d
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-06-25 09:23:29 +0300

    Strategy Pattern

---
+++ WepApp.Strategy/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WepApp.Strategy.Models
+{
+    public class AppUser:IdentityUser
+    {
+    }
+}
---
+++ WepApp.Strategy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WepApp.Strategy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

f2ae6c4ad80eb54599719239df79ad4b86aba8e0
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-06-26 16:48:42 +0300

    3.Adım

---
+++ WebApp.Adapter/Services/IAdvanceImageProcess.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace WebApp.Adapter.Services
+{
+    public interface IAdvanceImageProcess
+    {
+        // void AddWatermark(string text, string fileName, Stream imageStream);
+
+        void AddWatermarkImage(Stream stream, string text, string filePath, Color color, Color outlineColor);
+    }
+}
